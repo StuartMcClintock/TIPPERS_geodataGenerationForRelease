8054456e393de7866ebd3673f3351ca808503fed
Stuart McClintock <Stuart.e.mcc@gmail.com>
2020-04-15 18:30:03 -0700

    add protection against duplicate posts of same entity

--- pushData.py
+++ pushData.py
@@ -7,5 +7,15 @@ DEST_URL = 'http://dev-tippers.ics.uci.edu/api/entity/'
 with open(INPUT_FILE, 'r') as f:
     data = json.load(f)
 
+req = requests.get(DEST_URL)
+preexistingEntities = json.loads(req.text)
+takenNames = []
+for ent in preexistingEntities:
+    takenNames.append(ent["name"])
+
 for datum in data:
-    print(requests.post(DEST_URL, json=datum))
+    if not datum["name"] in takenNames:
+        print('POST request to add '+datum['name']+'...')
+        print(requests.post(DEST_URL, json=datum))
+    else:
+        print(datum['name']+' has already been posted.')
