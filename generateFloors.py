6cc48617d68027b3623c2048559299b594fb88a5
Stuart McClintock <Stuart.e.mcc@gmail.com>
2020-04-18 15:49:38 -0700

    set up id system using both name and parent id to allow functionality with all entity types

--- generateFloors.py
+++ generateFloors.py
@@ -6,49 +6,78 @@ from buildDB import SPACETABLENAME
 DEST_URL = 'https://dev-tippers.ics.uci.edu/api/entity/'
 SOURCE_URL = 'https://dev-tippers.ics.uci.edu/api/entity/'
 
-def getIdAndName():
+ID_OF_DESIRED_OBJ_TYPE = 7
+ID_OF_PARENT_OBJ = 5
+
+def getIdAndNameBuilding():
     data = requests.get(SOURCE_URL).json()
     nameDict = {}
     for datum in data:
-        nameDict[datum["name"]] = datum["id"]
+        if datum["entityTypeId"] == ID_OF_PARENT_OBJ:
+            nameDict[datum["name"]] = datum["id"]
     return nameDict
 
-def buildFloorObjects():
+def getIdAndPair():
+    data = requests.get(SOURCE_URL).json()
+    nameDict = {}
+    for datum in data:
+        if datum["entityTypeId"] == ID_OF_DESIRED_OBJ_TYPE:
+            nameDict[(datum["name"], datum["payload"]["geo"]["parentSpaceId"])] = datum["id"]
+    return nameDict
+
+def buildEntityObjects():
     con = sqlite3.connect('spaces.db')
     cursor = con.cursor()
+    cursor.execute("SELECT space_id, space_name, floor_id, building_id FROM "+SPACETABLENAME+" WHERE space_type='Room';")
+    subData = cursor.fetchall()
+    
     cursor.execute("SELECT space_id, space_name, building_id FROM "+SPACETABLENAME+" WHERE space_type='Floor';")
-    floorData = cursor.fetchall()
+    superData = cursor.fetchall()
+    #print(superData)
+    
     cursor.execute("SELECT space_id, space_name FROM "+SPACETABLENAME+" WHERE space_type='Building';")
-    nameList = cursor.fetchall()
+    buildingData = cursor.fetchall()
+    
     con.close()
-    idMap = getIdAndName()
+    idMap = getIdAndPair()
+    
+    buildingNameMap = {}
+    for building in buildingData:
+        buildingNameMap[building[0]] = building[1]
+    
+    buildingIds = getIdAndNameBuilding()
     
     nameMap = {}
-    for building in nameList:
-        nameMap[building[0]] = building[1]
-    
-    #print(nameMap)
-    #print(idMap)
-    
-    floorObjects = []
-    for floor in floorData:
-        newFloor = {}
-        newFloor["name"] = "floor "+floor[1]
-        newFloor["entityTypeId"] = 7
+    for super in superData:
         try:
-            newFloor["payload"] = {"geo":{"parentSpaceId":idMap[nameMap[floor[2]]]}}
+            nameMap[(super[1], buildingIds[buildingNameMap[super[2]]])] = super[1]
+        except KeyError:
+            print("error with superspace(floor) "+str(super[0]))
+    
+    print(nameMap)
+    print('------------------------')
+    print(idMap)
+    print('------------------------')
+    
+    subObjects = []
+    for subSpace in subData:
+        newSubSpace = {}
+        newSubSpace["name"] = "floor "+subSpace[1]
+        newSubSpace["entityTypeId"] = 7
+        try:
+            newSubSpace["payload"] = {"geo":{"parentSpaceId":idMap[nameMap[subSpace[2]]]}}
         except KeyError:
             continue
-        floorObjects.append(newFloor)
+        subObjects.append(newFloor)
         
-    return floorObjects
+    return subObjects
 
 def main():
-    data = buildFloorObjects()
-    print(data)
-    for datum in data:
-        print(datum["name"])
-        print(requests.post(DEST_URL, json=datum))
+    data = buildEntityObjects()
+    #print(data)
+    #for datum in data:
+    #    print(datum["name"])
+    #    print(requests.post(DEST_URL, json=datum))
     
 if (__name__=='__main__'):
     main()
